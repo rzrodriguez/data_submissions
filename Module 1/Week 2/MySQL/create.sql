511cd739a1e982e4ee061c1e540f4821eb2e708b
rzrodriguez <rodrigo.zrodriguez@gmail.com>
2019-07-22 17:06:28 -0300

    Labs Mysql

--- Module 1/Week 2/MySQL/create.sql
+++ Module 1/Week 2/MySQL/create.sql
@@ -1,10 +1,43 @@
-USE lab_mysql;
-SHOW TABLES;
+CREATE DATABASE IF NOT EXISTS lab_mysql;
 
-CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1),
-birth DATE, death DATE);
+CREATE TABLE lab_mysql.Cars
+(
+	ID INT NOT NULL PRIMARY KEY,
+   VIN VARCHAR(50),
+   Manufacturer VARCHAR(50),
+   Model VARCHAR(50),
+   Year_Factor INT(4),
+   Color VARCHAR(50)
+);
 
-SHOW TABLES;
+CREATE TABLE lab_mysql.Customers
+(
+	ID INT NOT NULL,
+   Customer_ID varchar(17) PRIMARY KEY,
+   Cust_name varchar(50),
+   Phone VARCHAR(20),
+   Email varchar(30),
+   Address varchar(50),
+   City varchar(20),
+   State_Province varchar(15),
+   Country varchar(20),
+   Postal int(20)
+);
 
-DESCRIBE pet;
+CREATE TABLE lab_mysql.Salespersons
+(
+    ID INT NOT NULL PRIMARY KEY,
+   Staff_ID int(5),
+   Sale_name varchar(50),
+   Store VARCHAR(20)
+   );
 
+   CREATE TABLE lab_mysql.Invoices
+(
+    ID INT NOT NULL PRIMARY KEY,
+   Invoice_number int(10),
+   Invoice_date date,
+   Car int(3),
+   Customer int(6),
+   Sales_person int(6)
+);
--- Module 1/Week 2/MySQL/seeding.sql
+++ Module 1/Week 2/MySQL/seeding.sql
@@ -1,12 +1,27 @@
-USE lab_mysql;
-
-CREATE TABLE cars (car_id VARCHAR(20), VIN VARCHAR(20), Manufacturer VARCHAR(20), Model VARCHAR(20), Caryear INT, Color VARCHAR(20));
-
- CREATE TABLE customers (customer_id VARCHAR(20), Name VARCHAR(20), Phone VARCHAR(20), Email VARCHAR(20), Address VARCHAR(20), City VARCHAR(20), State/Province VARCHAR(20), 
- Country VARCHAR(20), Postal VARCHAR(20));
- 
- CREATE TABLE Salesperson (staff_id VARCHAR(20), Employee VARCHAR(20), Store VARCHAR(20));
- 
- CREATE TABLE Invoices (invoice_id VARCHAR(20), Sale_date DATE, car_id VARCHAR(20), customer_id VARCHAR(20), staff_id VARCHAR(20));
- 
- 
+INSERT INTO lab_mysql.Cars (ID, VIN, Manufacturer, Model, Year_factor, Color)
+	VALUES ('0', '3K096I98581DHSNUP', 'Volkswagen', 'Tiguan', '2019', 'Blue'),
+	('1', 'ZM8G7BEUQZ97IH46V', 'Peugeot', 'Rifter', '2019', 'Red'),
+	('2', 'RKXVNNIHLVVZOUB4M', 'Ford', 'Fusion', '2018', 'White'),
+	('3', 'HKNDGS7CU31E9Z7JW', 'Toyota', 'RAV4', '2018', 'Silver'),
+    ('4', 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60', '2019', 'Gray'),
+    ('5', 'DAM41UDN3CHU2WVF6', 'Volvo', 'V60 Cross Country', '2019', 'Gray');
+    
+INSERT INTO lab_mysql.Customers (ID, Customer_ID, Cust_name, Phone, Email, Address, City, State_Province, Country, Postal)
+	VALUES ('0', '10001', 'Pablo Picasso', '+34 636 17 63 82', '', 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045'),
+    ('1', '20001', 'Abraham Lincoln', '+1 305 907 7086', '', '120 SW 8th St', 'Miami', 'Florida', 'United States', '33130'),
+    ('2', '30001', 'Napoléon Bonaparte', '+33 1 79 75 40 00', '', '40 Rue du Colisée', 'Paris', 'Île-de-France', 'France', '75008');
+    
+INSERT INTO lab_mysql.Salespersons (ID, Staff_ID, Sale_name, Store)
+	VALUES ('0', '00001', 'Petey Cruiser', 'Madrid'),
+    ('1', '00002', 'Anna Sthesia', 'Barcelona'),
+    ('2', '00003', 'Paul Molive', 'Berlin'),
+    ('3', '00004', 'Gail Forcewind', 'Paris'),
+	('4', '00005', 'Paige Turner', 'Miami'),
+    ('5', '00006', 'Bob Frapples', 'Mexico City'),
+    ('6', '00007', 'Walter Melon', 'Amsterdam'),
+    ('7', '00008', 'Shonda Leer', 'São Paulo');
+    
+INSERT INTO lab_mysql.Invoices (ID, Invoice_number, Invoice_Date, Car, Customer, Sales_person)
+	VALUES('0', '852399038', '2018-08-22', '0', '1', '3'),
+    ('1', '731166526', '2018-12-31', '3', '0', '5'),
+    ('2', '271135104', '2019-01-22', '2', '2', '7');
